8e0f44ea28ba5bc51e4a55a15350e0a15bad9488
Saurav Pandey <149031244+sauravpandey538@users.noreply.github.com>
2024-07-02 07:05:50 +0545

    Update multer.js

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,6 +1,6 @@
 import multer from "multer";
 import fs from "fs"
-const uploadDir = 'temp/uploads';
+const uploadDir = 'tmp/uploads';
 
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
