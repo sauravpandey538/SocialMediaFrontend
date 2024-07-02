614543cfb6318ce97c96dd8a3eb83dc8f53b5305
Saurav Pandey <149031244+sauravpandey538@users.noreply.github.com>
2024-07-02 07:15:59 +0545

    Update multer.js

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,10 +1,6 @@
-import multer from "multer";
-// import fs from "fs"
-const uploadDir = 'uploads';
+import multer from 'multer';
 
-// if (!fs.existsSync(uploadDir)) {
-//     fs.mkdirSync(uploadDir, { recursive: true });
-// }
+const uploadDir = '/tmp'; // Use /tmp for temporary storage in Vercel
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
